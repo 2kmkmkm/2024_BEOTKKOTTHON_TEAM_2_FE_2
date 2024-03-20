d368a5bda4ce8e79224b15938a700af6e48e27da
2kmkm <02dlrudals@gmail.com>
2024-03-20 23:49:39 +0900

    feat: 캘린더 구현

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import CalendarPage from "./Page/CalendarPage";
+
+function App() {
+  return (
+    <div>
+      <CalendarPage />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Page/CalendarPage.jsx
@@ -0,0 +1,39 @@
+import React, { useState } from "react";
+import Calendar from "react-calendar";
+import moment from "moment";
+import "./calendar.css";
+import catfoot from "../Img/catfoot.svg";
+
+function CalendarPage() {
+  const [value, onChange] = useState(new Date());
+  const dayList = ["2024-03-19"];
+
+  const addContent = ({ date, view }) => {
+    const content = [];
+    if (dayList.find((x) => x === moment(date).format("YYYY-MM-DD"))) {
+      content.push(<img src={catfoot} alt="" />);
+      return (
+        <>
+          <div>{content}</div>
+        </>
+      );
+    }
+  };
+
+  return (
+    <div>
+      <Calendar
+        formatDay={(locale, date) => moment(date).format("D")}
+        calendarType="gregory"
+        next2Label={null}
+        prev2Label={null}
+        onChange={onChange}
+        value={value}
+        tileContent={addContent}
+        minDetail="year"
+      />
+    </div>
+  );
+}
+
+export default CalendarPage;
